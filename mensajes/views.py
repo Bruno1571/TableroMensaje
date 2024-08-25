ea7a0b6926fe50868150698c2b3ca3680641e181
Bruno1571 <brunoconde29@gmail.com>
2024-08-25 20:09:12 -0300

    Creacion de la vista

--- mensajes/views.py
+++ mensajes/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Mensaje
 
-# Create your views here.
+def mensajes_recibidos(request):
+    # Obtener el destinatario 
+    destinatario = request.GET.get('destinatario', '')
+
+    # Filtrar los mensajes que tienen el destinatario 
+    if destinatario:
+        mensajes = Mensaje.objects.filter(destinatario=destinatario)
+    else:
+        mensajes = Mensaje.objects.none()  
+
+   
+    return render(request, 'recibidos.html', {'mensajes': mensajes})
